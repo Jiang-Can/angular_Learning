3ac7ffd19a6d2d5ac2bb114e3537a6c8ee56d81a
Can.Jiang <can774899203@gmail.com>
2022-01-29 23:34:08 +1100

    First Chapter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewsComponent } from './components/news/news.component';
+import { FormsModule} from "@angular/forms";
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [  //配置当前项目运作的组件
+    AppComponent, NewsComponent
   ],
-  imports: [
+  imports: [  //当前项目所需的其他模块
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [],  //配置项目需要的服务
   bootstrap: [AppComponent]
 })
 export class AppModule { }
